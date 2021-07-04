055cedd8e0ed3800156d94d86526c1bf62042b43
yaizkazani <yaizkazani@gmail.com>
2021-07-04 19:08:35 +0300

    initial commit

---
+++ swap_release.py
@@ -0,0 +1,6 @@
+import os, time, subprocess, pathlib
+
+def get_media_server_list():
+	media_server_list = subprocess.run([r"cat exec $(locate bp.conf | sed -n '1 p') | egrep '^MEDIA_SERVER'"], shell=True)
+
+print(get_media_server_list())
