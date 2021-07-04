f3cefa1964b5e85e85f6067386e2f5fdc27903be
yaizkazani <yaizkazani@gmail.com>
2021-07-04 19:14:59 +0300

    test

--- swap_release.py
+++ swap_release.py
@@ -4,3 +4,5 @@ def get_media_server_list():
 	media_server_list = subprocess.run([r"cat exec $(locate bp.conf | sed -n '1 p') | egrep '^MEDIA_SERVER'"], shell=True)
 
 print(get_media_server_list())
+
+#test
