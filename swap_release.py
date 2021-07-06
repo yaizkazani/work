74f2e8709941912c4a7160a01ce3d2e813031e11
Dkpidmfujdkplocal2 <Dkpidmfujdkplocal2@dk-prod-nbujum.dkp.local>
2021-07-06 11:02:14 +0200

    fixed media server list func

--- swap_release.py
+++ swap_release.py
@@ -1,8 +1,8 @@
 import os, time, subprocess, pathlib
 
 def get_media_server_list():
-	media_server_list = subprocess.run([r"cat exec $(locate bp.conf | sed -n '1 p') | egrep '^MEDIA_SERVER'"], shell=True).stdout
-	return media_server_list
+	media_server_list = subprocess.getoutput(r"cat exec $(locate bp.conf | sed -n '1 p') | egrep '^MEDIA_SERVER'")
+	return [item.split("=")[1].strip() for item in media_server_list.split("\n") if "MEDIA_SERVER" in item]
 
 print(get_media_server_list())
 
