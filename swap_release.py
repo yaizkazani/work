1f3327ad70d02a138367931d3d464bd88174ab08
yaizkazani <yaizkazani@gmail.com>
2021-07-06 11:46:18 +0300

    test3

--- swap_release.py
+++ swap_release.py
@@ -1,7 +1,8 @@
 import os, time, subprocess, pathlib
 
 def get_media_server_list():
-	media_server_list = subprocess.run([r"cat exec $(locate bp.conf | sed -n '1 p') | egrep '^MEDIA_SERVER'"], shell=True)
+	media_server_list = subprocess.run([r"cat exec $(locate bp.conf | sed -n '1 p') | egrep '^MEDIA_SERVER'"], shell=True).stdout
+	return media_server_list
 
 print(get_media_server_list())
 
